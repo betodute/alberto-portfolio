052b2d6d0ddfdb77e8a3ea55c574c3e030c8a881
Alberto Duarte <betodute@gmail.com>
2025-01-12 00:20:17 -0800

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <div className='header-wrapper'> 
+      <div className='header-title'>
+        <div className='museo'>
+          Museo Moderno : W w M m N n P p S s Q q G g R r X x Y y
+        </div>
+        <div className='grenze'>
+          Grenze : H h O o Q q Z z S s
+        </div>
+        <div className='chivo'> Chivo: Y y T t F f M m K k</div>
+      </div>
+    </div>
+  )
+}
