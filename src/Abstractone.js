d122d30c079880a0d2c02fca9369bdcd8f3c470a
Alberto Duarte <betodute@gmail.com>
2025-03-06 18:54:10 -0800

    fixed navbar work on parallax next

--- src/Abstractone.js
+++ src/Abstractone.js
@@ -4,7 +4,6 @@ import './Abstractone.css'
 export const Abstractone = () => {
   return (
     <div className='abstractone-wrapper'>
-      <div className='figure first' />
       <div className='figure second' />
       <div className='figure third' />
     </div>
